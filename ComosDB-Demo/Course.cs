5cc8df71d368082b8784e04776454dbb0153188e
fellly <fellyka@gmail.com>
2023-06-18 13:52:21 +0200

    Add an Item

---
+++ ComosDB-Demo/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ComosDB_Demo
+{
+    public class Course
+    {
+        public string id { get; set; }
+        public string courseId { get; set; }
+        public string courseName { get; set; }
+        public decimal rating { get; set; }
+    }
+}
--- ComosDB-Demo/Program.cs
+++ ComosDB-Demo/Program.cs
@@ -9,18 +9,21 @@ namespace ComosDB_Demo
         private static readonly string connectionString = "AccountEndpoint=https://cos-azsollers.documents.azure.com:443/;AccountKey=nbasL34Pa3MRaH5fEvAlkmbidTzk0GaPzNPLvsVIDpRVnK3tCE95bIzex2YMSPJ2sZSRIDYxzJ6kACDbCZY0SA==;";
         private static readonly string databaseName = "TestDataBase";
         private static readonly string containerName = "TestContainer";
-        private static readonly string partitionKey = "/courseid";
+       
 
         static void Main(string[] args)
         {
             CosmosClient client = new CosmosClient(connectionString);
-            client.CreateDatabaseAsync(databaseName).GetAwaiter().GetResult();
-            //Operation on database
-            Database database = client.GetDatabase(databaseName);
+            Course course = new Course()
+            {
+                 id = "A1", courseId = "AZ-900", courseName = "Azure Fundamentals", rating = 4.5m
+            };
 
-            database.CreateContainerAsync(containerName, partitionKey).GetAwaiter().GetResult();
+            Container container = client.GetContainer(databaseName, containerName);
+            container.CreateItemAsync<Course>(course, new PartitionKey(course.courseId)).GetAwaiter();//.GetResult();
+          
 
-            Console.WriteLine("Database and container have been created");
+            Console.WriteLine("Course item has been created");
             Console.ReadKey();
         }
     }
